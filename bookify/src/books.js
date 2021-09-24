27a722354e5a02e3789ac3890bb08bb06a40097c
Sahil <sahilsarin390@gmail.com>
2021-09-25 00:54:43 +0530

    Basic Structure done

---
+++ bookify/src/books.js
@@ -0,0 +1,18 @@
+export const data = [
+
+  { id:1,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
+    title:'Ikigai',
+    author:'Héctor García'
+  },
+  { id:2,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
+    title:'The Psychology of Money',
+    author:'Morgan Housel'
+  },
+  { id:3,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg',
+    title:'Atomic Habits',
+    author:'James Clear'
+  }
+];
--- bookify/src/index.js
+++ bookify/src/index.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const books = [
-
-  { id:1,
-    img:'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
-    title:'Ikigai',
-    author:'Héctor García'
-  },
-  { id:2,
-    img:'https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
-    title:'The Psychology of Money',
-    author:'Morgan Housel'
-  },
-  { id:3,
-    img:'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg',
-    title:'Atomic Habits',
-    author:'James Clear'
-  }
-];
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing.js';
 
 function BookList(){
+  console.log(greeting)
 return (
     <section className='booklist'>
-      {books.map((book, index) =>{
-        return <Book key={book.id} {...book}></Book>;
+      {data.map((book, index) =>{
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
   );
 }
-
-const Book = ({img, title, author}) => {
-  return (
-    <article className='book'>
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDOM.render(
   <BookList/>,
